b4f4947b369c02d3f9ab43353e22fa367b5888dd
zoyul <p.zoyul@gmail.com>
2024-10-12 11:40:45 +0900

    feat: Add order controller(Collection Query Optimization 2 -using  fetch join and hibernate batch)

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.repository.OrderRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -44,6 +45,18 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream()
+                .map(OrderDto::new)
+                .toList();
+    }
+
+    // toOne 관계는 모둔 페치 조인 후 컬렉션은 지연로딩
+    // 지연로딩 최적화를 위해 hibernate.default_batch_fetch_size 사용
+    // 개별로 설정하려면 @BatchSize 적용 (컬렉션은 컬렉션 필ㄷ에, 엔티티는 엔티티 클랫에 적용)
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         return orders.stream()
                 .map(OrderDto::new)
                 .toList();
