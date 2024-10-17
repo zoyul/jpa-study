9aeae6e218d076e90ef723dcc98792f5f9de7b47
zoyul <p.zoyul@gmail.com>
2024-10-18 00:09:37 +0900

    feat: spring data jpa and database setting, and basic operations
    - Created Entity and Repository
    - Implemented simple CRUD tests

---
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,23 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    protected Member() {
+    }
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,29 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional      // JPA의 모든 데이터 변경은 transaction 안에서 일어나야 한다
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+        assertThat(findMember).isEqualTo(member);           // 같은 transaction 안에서는 같은 영속성 컨텍스트의 데이터임을 보장한다
+    }
+
+}
