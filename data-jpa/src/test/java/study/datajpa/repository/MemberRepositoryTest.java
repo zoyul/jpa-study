4fffd9b05d688c839284a6537a7810361ee3c1ab
zoyul <p.zoyul@gmail.com>
2024-10-19 23:16:47 +0900

    feat: Add method using collection parameter binding

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -131,4 +131,16 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getTeamName()).isEqualTo("teamA");
         assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(List.of("AAA", "BBB"));
+
+        assertThat(result.size()).isEqualTo(2);
+    }
 }
