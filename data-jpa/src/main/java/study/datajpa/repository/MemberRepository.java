0cd66575a2fc51ec3fb58e9fcdd4a21287486842
zoyul <p.zoyul@gmail.com>
2024-10-19 22:21:09 +0900

    fea: Add jpa query method

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 // interface를 선언하면 spring data jpa가 구현체를 직접 주입해줌
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
