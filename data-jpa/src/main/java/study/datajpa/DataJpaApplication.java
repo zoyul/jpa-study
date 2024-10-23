6146ac7a3d1a5a1de7d710d0b66f48b79bb03427
zoyul <p.zoyul@gmail.com>
2024-10-24 02:12:37 +0900

    feat: Add JpaAuditing feature

--- data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,14 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing  // (modifyOnCreate = false) 설정하면 create할 때 update 값에 null이 ㄷㄹ어감
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -10,4 +17,8 @@ public class DataJpaApplication {
         SpringApplication.run(DataJpaApplication.class, args);
     }
 
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -3,9 +3,11 @@ package study.datajpa.entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import java.util.List;
 
@@ -16,6 +18,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -46,4 +51,26 @@ class MemberTest {
         });
     }
 
+    @Test
+    void JpaEventBaseEntity() {
+        // given
+        Member member = new Member("member1");
+        memberRepository.save(member);
+        em.flush();
+        em.clear();
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        findMember.setUsername("member2");
+        em.flush();
+        em.clear();
+
+        // when
+        Member updatedMember = memberRepository.findById(member.getId()).get();
+
+        // then
+        System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+        System.out.println("updatedMember.updatedDate = " + updatedMember.getLastModifiedDate());
+        System.out.println("findMember.getCreatedBy = " + updatedMember.getCreatedBy());
+        System.out.println("updatedMember.getLastModifiedBy = " + updatedMember.getLastModifiedBy());
+    }
 }
